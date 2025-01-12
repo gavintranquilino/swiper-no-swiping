a6c75eada8c7cadcdd5f399fddbb470ef1c68445
ryanguo2005 <ryanguo2005@gmail.com>
2025-01-11 21:28:40 -0500

    I made HelloWorld. im so smart

---
+++ onboard/weapon-detect/SwiperNoSwiping.py
@@ -0,0 +1,31 @@
+import serial
+import time
+import pygame
+
+# Configure the serial connection
+arduino = serial.Serial(port='COM5', baudrate=9600, timeout=1)  # Replace 'COM3' with your Arduino port
+time.sleep(2)  # Wait for the connection to initialize
+pygame.mixer.init()
+pygame.mixer.music.load("Swiper no swiping.mp3")
+
+swiped = False
+
+try:
+    while True:
+        # Read data from Arduino
+        if arduino.in_waiting > 0:
+            line = arduino.readline().decode('utf-8').strip()
+            print(line)
+            if line == "1":
+                swiped = True
+
+        if swiped == True:
+            print('Swiper no Swiping!')
+            pygame.mixer.music.play()
+        swiped = False
+
+        # time.sleep(1  # Wait for 1 second
+except KeyboardInterrupt:
+    print("Communication stopped.")
+finally:
+    arduino.close()
