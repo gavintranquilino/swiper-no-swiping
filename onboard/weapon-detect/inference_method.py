e26114c52c4c3e1507760b28cf8d2abd60e06f24
gavintranquilino <gavintranquilino@gmail.com>
2025-01-12 00:11:39 -0500

    Camera gavin's computer

--- onboard/weapon-detect/inference_method.py
+++ onboard/weapon-detect/inference_method.py
@@ -15,7 +15,7 @@ with open("package.json", "r") as f:
 # initialize a pipeline object
 pipeline = InferencePipeline.init(
     model_id="knife-detection-hgvy2/1", # Roboflow model to use
-    video_reference=0, # Path to video, device id (int, usually 0 for built in webcams), or RTSP stream url
+    video_reference=2, # Path to video, device id (int, usually 0 for built in webcams), or RTSP stream url
     on_prediction=do, # Function to run after each prediction
     api_key=key
 )
